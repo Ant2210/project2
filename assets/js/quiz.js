44ee5f9ff6a3a41428e116c2c36d8aff1561d233
Ant <ant.griffiths22@gmail.com>
2023-06-22 23:33:37 +0100

    Set const's got access to all required DOM elements

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,1 +1,13 @@
-console.log('Hello world!')
+const question = document.getElementById('question');
+const choices = document.getElementsByClassName('choices');
+const progress = document.getElementById('progress');
+const currentScore = document.getElementById('current-score');
+
+console.log(question.innerHTML)
+console.log(choices)
+console.log(choices[0].innerHTML)
+console.log(choices[1].innerHTML)
+console.log(choices[2].innerHTML)
+console.log(choices[3].innerHTML)
+console.log(currentScore.innerHTML)
+console.log(progress.innerHTML)
