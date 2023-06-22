6fd3bae7d4156d54b5aa9220f8e34a11fd40c716
Ant <ant.griffiths22@gmail.com>
2023-06-23 00:09:54 +0100

    Function to return  random question from available

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -32,19 +32,3 @@ newQuestion = () => {
 }
 
 newQuestion()
-
-
-
-
-
-
-
-
-// console.log(question.innerHTML)
-// console.log(choices)
-// console.log(choices[0].innerHTML)
-// console.log(choices[1].innerHTML)
-// console.log(choices[2].innerHTML)
-// console.log(choices[3].innerHTML)
-// console.log(currentScore.innerHTML)
-// console.log(progress.innerHTML)
