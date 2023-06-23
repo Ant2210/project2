0962748ca7c40827c72b10dca6db03a92c3b4604
Ant <ant.griffiths22@gmail.com>
2023-06-23 01:20:24 +0100

    Create check answer function

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,5 +1,5 @@
 const question = document.getElementById("question");
-const choices = document.getElementsByClassName("choices");
+const choices = Array.from(document.getElementsByClassName("choices"));
 const progress = document.getElementById("progress");
 const currentScore = document.getElementById("current-score");
 
@@ -25,22 +25,36 @@ let availableQuestions = [];
 let currentQuestion = [];
 let currentAnswers = [];
 
+startGame = () => {
+    newQuestion();
+    checkAnswer();
+}
+
 newQuestion = () => {
     availableQuestions = [...questions];
     let random = Math.floor(Math.random() * 3);
     currentQuestion = availableQuestions.splice(random, 1);
     question.innerHTML = currentQuestion[0].question;
 
-    currentAnswers = currentQuestion[0].incorrectAnswers.concat(currentQuestion[0].correctAnswer);
+    currentAnswers = currentQuestion[0].incorrectAnswers.concat(
+        currentQuestion[0].correctAnswer
+    );
+    // Randomise array code obtained here -> https://www.slingacademy.com/article/ways-to-shuffle-an-array-in-javascript/?utm_content=cmp-true
     currentAnswers.sort(() => Math.random() - 0.5);
 
     let i = 0;
     while (i < choices.length) {
         choices[i].innerHTML = currentAnswers[i];
         i++;
-    };
+    }
 };
 
-newQuestion();
+checkAnswer = () => {
+    choices.forEach((choice) => {
+        choice.addEventListener("click", (e) => {
+            e.target.innerHTML == currentQuestion[0].correctAnswer ? console.log("well done") : console.log("whoops");
+        })
+    })
+}
 
-console.log(choices);
+startGame();
