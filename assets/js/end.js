7e9754f46f3e06be24bcce32423dd847f12d2e42
Ant <ant.griffiths22@gmail.com>
2023-06-23 22:13:35 +0100

    Create validation modal

--- assets/js/end.js
+++ assets/js/end.js
@@ -1,9 +1,16 @@
 const saveButton = document.getElementById("save-btn");
+const launchValidationModal = document.getElementById("launch-validation-modal");
 
 saveScore = () => {
     saveButton.addEventListener("click", (e) => {
         e.preventDefault();
-        console.log("you clicked");
+        // Check the value of an input field found here -> https://stackoverflow.com/questions/3937513/javascript-validation-for-empty-input-field
+        const userInput = document.forms["score-form"]["username"].value;
+        if(userInput == null || userInput == "") {
+            launchValidationModal.click();
+        } else {
+            alert('Thanks')
+        }
     });
 };
 
