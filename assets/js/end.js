1211988a076ef4fcec354f0c52f95ae981eba8f2
Ant <ant.griffiths22@gmail.com>
2023-06-24 00:45:16 +0100

    Add highscores and redirect to homepage

--- assets/js/end.js
+++ assets/js/end.js
@@ -1,12 +1,20 @@
 const saveButton = document.getElementById("save-btn");
-const launchValidationModal = document.getElementById("launch-validation-modal");
+const launchValidationModal = document.getElementById(
+    "launch-validation-modal"
+);
+const launchCountdown = document.getElementById("launch-countdown-modal");
 const mostRecentScore = localStorage.getItem("mostRecentScore");
+
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+
 const finalScore = document.getElementById("final-score");
 finalScore.innerHTML = mostRecentScore;
 
 const finalScoreEmoji = document.getElementsByClassName("final-score-emoji");
 Array.from(finalScoreEmoji).forEach((emoji) => {
-    (parseInt(mostRecentScore) === 0) ? emoji.innerHTML = "&#128557" : emoji.innerHTML = "&#127941";
+    parseInt(mostRecentScore) === 0
+        ? (emoji.innerHTML = "&#128557")
+        : (emoji.innerHTML = "&#127941");
 });
 
 saveScore = () => {
@@ -14,10 +22,33 @@ saveScore = () => {
         e.preventDefault();
         // Check the value of an input field found here -> https://stackoverflow.com/questions/3937513/javascript-validation-for-empty-input-field
         const userInput = document.forms["score-form"]["username"].value;
-        if(userInput == null || userInput == "") {
+        if (userInput == null || userInput == "") {
             launchValidationModal.click();
-        };
+        } else {
+            const score = {
+                score: Math.floor(Math.random() * 100),
+                name: userInput,
+            };
+
+            highScores.push(score);
+            highScores.sort((a, b) => b.score - a.score);
+
+            localStorage.setItem("highScores", JSON.stringify(highScores));
+            redirectToGame();
+        }
     });
 };
 
-saveScore()
+redirectToGame = () => {
+    let timeLeft = 10;
+    launchCountdown.click();
+    let downloadTimer = setInterval(() => {
+        timeLeft--;
+        document.getElementById("countdown-timer").innerHTML = timeLeft;
+        if (timeLeft <= 0) {
+            return window.location.assign("index.html");
+        }
+    }, 1000);
+};
+
+saveScore();
